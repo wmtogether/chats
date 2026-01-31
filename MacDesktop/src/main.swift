5d66fff148eae2576467d2481908d54d577b6c4c
arizkami <amcpethai22@gmail.com>
2026-01-31 16:02:00 +0700

    2

--- MacDesktop/src/main.swift
+++ MacDesktop/src/main.swift
@@ -757,13 +757,13 @@ class MainWindowController: NSWindowController {
         let editMenuItem = NSMenuItem(title: "Edit", action: nil, keyEquivalent: "")
         let editMenu = NSMenu(title: "Edit")
         
-        editMenu.addItem(withTitle: "Undo", action: #selector(NSText.undo(_:)), keyEquivalent: "z")
-        editMenu.addItem(withTitle: "Redo", action: #selector(NSText.redo(_:)), keyEquivalent: "Z")
+        editMenu.addItem(withTitle: "Undo", action: Selector(("undo:")), keyEquivalent: "z")
+        editMenu.addItem(withTitle: "Redo", action: Selector(("redo:")), keyEquivalent: "Z")
         editMenu.addItem(NSMenuItem.separator())
-        editMenu.addItem(withTitle: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x")
-        editMenu.addItem(withTitle: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c")
-        editMenu.addItem(withTitle: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v")
-        editMenu.addItem(withTitle: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a")
+        editMenu.addItem(withTitle: "Cut", action: Selector(("cut:")), keyEquivalent: "x")
+        editMenu.addItem(withTitle: "Copy", action: Selector(("copy:")), keyEquivalent: "c")
+        editMenu.addItem(withTitle: "Paste", action: Selector(("paste:")), keyEquivalent: "v")
+        editMenu.addItem(withTitle: "Select All", action: Selector(("selectAll:")), keyEquivalent: "a")
         
         editMenuItem.submenu = editMenu
         mainMenu.addItem(editMenuItem)
